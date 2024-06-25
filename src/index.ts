9cf915d0e3013e48a22557569f6a8c5123e462c4
Rafael Carvacho <110637459+Unnunoctio@users.noreply.github.com>
2024-06-25 19:10:43 -0400

    feat: :tada: started express server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-console.log("Hello via Bun!");
+import express from 'express'
+import { PORT } from './config'
+import { customLogger } from './middlewares/logger'
+
+// TODO: Configure Express
+const app = express()
+app.disable('x-powered-by')
+
+// TODO: Configure Middlewares
+app.use(express.json())
+app.use(customLogger)
+
+// TODO: Configure Routes
+
+// TODO: Handler not found route
+app.use((_, res) => {
+  return res.status(404).send({
+    error: 'The requested route does not exist in this API'
+  })
+})
+
+// TODO: Start Server
+app.listen(PORT, () => {
+  console.log(`Server started on port: ${PORT}`)
+})
