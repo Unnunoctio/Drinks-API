c0c777d3b6cd75b34b0de810f0ffbcffb64c0c99
Rafael Carvacho <110637459+Unnunoctio@users.noreply.github.com>
2024-06-25 18:34:03 -0400

    feat: :wrench: added env variables

---
+++ src/config.ts
@@ -0,0 +1,4 @@
+/* eslint-disable @typescript-eslint/strict-boolean-expressions */
+export const PORT = Number(Bun.env.PORT) || 3000
+export const API_KEY = Bun.env.API_KEY
+export const DB_URI = Bun.env.DB_URI
