1e5afa850af8c5ac9cc6711ffd030435359c5001
Rafael Carvacho <110637459+Unnunoctio@users.noreply.github.com>
2024-06-27 13:34:05 -0400

    feat: :sparkles: added save many drinks route

--- src/controllers/drink-controller.ts
+++ src/controllers/drink-controller.ts
@@ -1,7 +1,11 @@
-import { ZodError } from 'zod'
 import type { Request, Response } from 'express'
+import path from 'path'
+import { ZodError } from 'zod'
 import { validatePagination } from '../validations/pagination-validation'
-import { findDrinks } from '../services/drink-service'
+import { drinkService } from '../services/drink-service'
+import * as XLSX from 'xlsx'
+import type { IDrink, UDrink } from '../types'
+import { validateDrink } from '../validations/drink-validation'
 
 export const getDrinks = async (req: Request, res: Response): Promise<Response> => {
   try {
@@ -9,7 +13,7 @@ export const getDrinks = async (req: Request, res: Response): Promise<Response> 
     if (pagination.error !== undefined) throw new ZodError(pagination.error.issues)
 
     const { page, limit } = pagination.data
-    const drinks = await findDrinks()
+    const drinks = await drinkService.findDrinks()
 
     return res.status(200).json({
       data: drinks,
@@ -26,6 +30,48 @@ export const getDrinks = async (req: Request, res: Response): Promise<Response> 
         error: error.issues.map((err) => { return { path: err.path[0], message: err.message } })
       })
     }
+    return res.status(500).json({ error: error.message })
+  }
+}
+
+export const addManyDrinks = async (req: Request, res: Response): Promise<Response> => {
+  try {
+    if (req.file === undefined) throw new Error('No file provided')
+
+    const extension = path.extname(req.file.originalname)
+    const validExtensions = ['.xlsx', '.xls', '.csv']
+    if (!validExtensions.includes(extension)) throw new TypeError(JSON.stringify({ path: 'file', message: 'Invalid file extension', extensions: validExtensions }))
+
+    const workbook = XLSX.read(req.file.buffer, { type: 'buffer' })
+    const sheetNames = workbook.SheetNames
+
+    const newDrinks: IDrink[] = []
+    const updateDrinks: UDrink[] = []
+
+    for (const sheetName of sheetNames) {
+      if (sheetName === 'Valores' || sheetName === 'Template') continue
+      const worksheet = workbook.Sheets[sheetName]
+      const sheetData = XLSX.utils.sheet_to_json<any>(worksheet)
+
+      for (const data of sheetData) {
+        const { _id, name, brand, grade, contents, package: pack, category, subCategory, madeIn, variety, bitterness, temperature, strain, vineyard } = data
+        const contentsArray: number[] = contents.toString().split(',').map(Number).sort((a: number, b: number) => a - b)
+        const drink = validateDrink({ name, brand, alcoholicGrade: grade, contents: contentsArray, package: pack, category, subCategory, madeIn, variety, bitterness, temperature, strain, vineyard })
+        if (drink.error !== undefined) continue
+        if (_id !== undefined) {
+          updateDrinks.push({ _id, ...drink.data })
+        } else {
+          newDrinks.push(drink.data)
+        }
+      }
+    }
+
+    const drinksAdded = await drinkService.saveManyDrinks(newDrinks)
+
+    return res.status(200).json({ data: { drinksAdded: drinksAdded.length } })
+  } catch (error: any) {
+    if (error instanceof TypeError) return res.status(400).json({ error: JSON.parse(error.message) })
+    if (error instanceof Error) return res.status(400).json({ error: error.message })
     return res.status(500).json({ error: error.message })
   }
 }
--- src/routes/drink-routes.ts
+++ src/routes/drink-routes.ts
@@ -1,11 +1,14 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
+import multer from 'multer'
 import { Router } from 'express'
-import { getDrinks } from '../controllers/drink-controller'
+import { addManyDrinks, getDrinks } from '../controllers/drink-controller'
 
+const storage = multer.memoryStorage()
+const upload = multer({ storage })
 const router = Router()
 
 router.get('/drinks', getDrinks)
 
-router.post('/drinks/excel')
+router.post('/drinks/excel', upload.single('file'), addManyDrinks)
 
 export default router
