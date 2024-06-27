b89e56b86866f7a9fed4c91ab123e03502cbab78
Rafael Carvacho <110637459+Unnunoctio@users.noreply.github.com>
2024-06-27 13:28:32 -0400

    feat: :card_file_box: updated drinks database service

--- src/services/drink-service.ts
+++ src/services/drink-service.ts
@@ -1,6 +1,27 @@
-import DrinkModel from '../models/drink-model'
-import type { IDrink } from '../types'
+import DrinkModel, { type MDrink } from '../models/drink-model'
+import type { IDrink, UDrink } from '../types'
 
-export const findDrinks = async (): Promise<IDrink[]> => {
+const findDrinks = async (): Promise<IDrink[]> => {
   return await DrinkModel.find({}, { _id: 0, createdAt: 0, updatedAt: 0 }).lean().exec()
+}
+
+const saveManyDrinks = async (drinks: IDrink[]): Promise<MDrink[]> => {
+  const filtered = (await Promise.all(drinks.map(async (d) => {
+    const isExists = await DrinkModel.exists({ name: d.name, brand: d.brand, alcoholicGrade: d.alcoholicGrade, package: d.package })
+    if (isExists !== null) return null
+    return d
+  }))).filter((d) => d !== null)
+
+  const drinksAdded = await DrinkModel.insertMany(filtered)
+  return drinksAdded
+}
+
+const updateManyDrinks = async (drinks: UDrink[]): Promise<void> => {
+  // await DrinkModel.insertMany(drinks)
+}
+
+export const drinkService = {
+  findDrinks,
+  saveManyDrinks,
+  updateManyDrinks
 }
