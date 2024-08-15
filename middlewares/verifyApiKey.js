3355d09891b1bf7d0d579edbaac4f854ca2c0382
Rafael Carvacho <110637459+Unnunoctio@users.noreply.github.com>
2024-08-15 19:18:45 -0400

    feat: :fire: deleted deprecate files

--- middlewares/verifyApiKey.js
+++
@@ -1,9 +0,0 @@
-const verifyApiKey = (req, res, next) => {
-  const apiKey = req.headers['x-api-key']
-  if (apiKey !== process.env.API_KEY) {
-    return res.status(401).sendResponse({ error: { message: 'Unauthorized' } })
-  }
-  next()
-}
-
-export default verifyApiKey
