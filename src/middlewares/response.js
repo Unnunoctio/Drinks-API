3ad14c355e6ffb7790d53556f8a4844b4a7e45ee
Rafael Carvacho <110637459+Unnunoctio@users.noreply.github.com>
2024-08-26 00:27:27 -0400

    chore: :art: change response version

--- src/middlewares/response.js
+++ src/middlewares/response.js
@@ -13,7 +13,7 @@ export const formatResponse = async (c, next) => {
     error: isOk ? null : { code: status, message: body.error },
     meta: {
       timestamp: new Date().toISOString(),
-      version: '3.0',
+      version: '2.0',
       pagination: body.pagination
     }
   }
